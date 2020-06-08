29ae487e2bfc8363e52bb594db5a68f13489ab62
Tom <tom@tdpain.net>
2020-06-08 11:17:44 +0100

    Add image generation function

--- imageNoise/MainWindow.xaml.cs
+++ imageNoise/MainWindow.xaml.cs
@@ -1,13 +1,55 @@
-﻿namespace imageNoise
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace imageNoise
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private const int ImageWidth = 320;
+        private const int ImageHeight = 240;
+        private readonly Random RandomGenerator = new Random();
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            noiseImageViewer.Source = GenerateNoiseImage();
+
+        }
+
+        private BitmapImage GenerateNoiseImage()
+        {
+            var generatedImage = new Bitmap(ImageWidth, ImageHeight);
+            var convertedImage = new BitmapImage();
+            
+            for (var x = 0; x < generatedImage.Width; x++)
+            {
+                for (var y = 0; y < generatedImage.Height; y++)
+                {
+                    var color = RandomGenerator.Next(2) == 0 ? Color.Black : Color.White;
+                    generatedImage.SetPixel(x, y, color);
+                }
+            }
+
+            using (var memory = new MemoryStream())
+            {
+                generatedImage.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+                
+                convertedImage.BeginInit();
+                convertedImage.StreamSource = memory;
+                convertedImage.CacheOption = BitmapCacheOption.OnLoad;
+                convertedImage.EndInit();
+            }
+            
+            return convertedImage;
+            
         }
     }
 }

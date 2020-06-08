58ec156b9238059862f70efbbaa3f77ad993a247
Tom <tom@tdpain.net>
2020-06-08 13:14:40 +0100

    Add image noise challenge (#1)
    * Add base stuff
    
    * Untrack everything
    
    * Add C# specific gitignore
    
    * Add base files
    
    * Add main window XAML
    
    * Add reference System.Drawing
    
    * Tweak window size
    
    * Add image generation function
    
    * Animate noise
    
    * Tidy and add FPS count
    
    * Add README
    
    * Add release binary
    
    Signed-off-by: Tom <tom@tdpain.net>
    
    * Update README
    
    * Add image
    
    * Update README.md

---
+++ imageNoise/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace imageNoise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        private readonly DispatcherTimer Timer = new DispatcherTimer();
+        
+        private const int ImageWidth = 320;
+        private const int ImageHeight = 240;
+        private readonly Random _randomGenerator = new Random();
+
+        private DateTime FpsTimer = DateTime.Now;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            worker.DoWork += delegate
+            {
+                Dispatcher.BeginInvoke((Action) (() => { noiseImageViewer.Source = GenerateNoiseImage(); }));
+            };
+            worker.RunWorkerCompleted += UpdateFps;
+
+            void OnTick(object sender, object e)
+            {
+                try
+                {
+                    worker.RunWorkerAsync();
+                } catch (InvalidOperationException) { }  // previous tick has not finished
+            }
+           
+            Timer.Interval = TimeSpan.FromMilliseconds(16);  // 60fps maximum
+            Timer.Tick += OnTick;
+
+            Timer.Start();
+        }
+
+        private BitmapImage GenerateNoiseImage()
+        {
+            var generatedImage = new Bitmap(ImageWidth, ImageHeight);
+            var convertedImage = new BitmapImage();
+            
+            for (var x = 0; x < generatedImage.Width; x++)
+            {
+                for (var y = 0; y < generatedImage.Height; y++)
+                {
+                    var color = _randomGenerator.Next(2) == 0 ? Color.Black : Color.White;
+                    generatedImage.SetPixel(x, y, color);
+                }
+            }
+
+            using (var memory = new MemoryStream())
+            {
+                generatedImage.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+                
+                convertedImage.BeginInit();
+                convertedImage.StreamSource = memory;
+                convertedImage.CacheOption = BitmapCacheOption.OnLoad;
+                convertedImage.EndInit();
+            }
+            
+            return convertedImage;
+            
+        }
+
+        private void UpdateFps(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var now = DateTime.Now;
+            var fps = Math.Round(1 / (now - FpsTimer).TotalSeconds, 0);
+            FpsTimer = now;
+
+            Dispatcher.BeginInvoke((Action) (() => { fpsIndicator.Text = fps.ToString(); }));
+        }
+        
+    }
+}
---
+++ imageNoise/README.md
@@ -0,0 +1,20 @@
+# Image noise
+
+http://rosettacode.org/wiki/Image_noise
+
+```
+Date: 08/06/2020
+Language: C#
+Lines: 122
+Status: could be improved
+```
+
+#### Problems
+
+* Inefficient
+  * Averages around 10fps
+  * Could be improved with better looping and threading
+  
+#### Images
+
+![Screenshot](https://raw.githubusercontent.com/codemicro/codeChallenges/master/imageNoise/image.png)

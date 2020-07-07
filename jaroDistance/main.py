e7c02455c3ad189cf0c2aa6fc4a7c2c112b3e13c
Tom <tom@tdpain.net>
2020-07-07 20:13:15 +0100

    Clean up (remove debug prints and unused function)
    Signed-off-by: Tom <tom@tdpain.net>

--- jaroDistance/main.py
+++ jaroDistance/main.py
@@ -1,15 +1,3 @@
-def pad_arrays(l1, l2):
-  l1_len = len(l1)
-  l2_len = len(l2)
-
-  if l1_len > l2_len:
-    for _ in range(l1_len-l2_len):
-      l2.append(None)
-  elif l2_len > l1_len:
-    for _ in range(l2_len-l1_len):
-      l1.append(None)
-
-
 def jaro_similarity(s1:str, s2:str) -> float:
   s1_len = len(s1)
   s2_len = len(s2)
@@ -21,7 +9,6 @@ def jaro_similarity(s1:str, s2:str) -> float:
     return 1.0
 
   match_distance_requirement = int((s1_len if s1_len > s2_len else s2_len) / 2) - 1
-  print(match_distance_requirement)
 
   matches = 0
   transpositions = 0
@@ -35,7 +22,6 @@ def jaro_similarity(s1:str, s2:str) -> float:
     upper_bound = min(s2_len, i + match_distance_requirement + 1)
     
     for x in range(lower_bound, upper_bound):
-      print(lower_bound, "-", upper_bound, "@", x, f"({i})", s1[i], s2[x], s1[i] == s2[x])
       if s2_matches[x]:  # Already matched to something, so cannot use
         continue
       elif s1[i] == s2[x]:
@@ -43,8 +29,6 @@ def jaro_similarity(s1:str, s2:str) -> float:
         s2_matches[x] = True
         matches += 1
         break  # match found, no need to keep iterating
-
-  print(s1, s1_matches, s2, s2_matches)
 
   # Find number of transpositions
   x = 0
@@ -65,8 +49,6 @@ def jaro_similarity(s1:str, s2:str) -> float:
     x += 1
 
   transpositions /= 2
-
-  print("m", matches, "t", transpositions)
 
   # Calulate similarity
   sim = 1/3*((matches/s1_len) + (matches/s2_len) + ((matches-transpositions)/matches)) if matches != 0 else 0.0

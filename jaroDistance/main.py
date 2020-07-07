6bef86b368bbc4f7756c443cd6fa77b44b929089
Tom <tom@tdpain.net>
2020-07-07 20:06:05 +0100

    Attempt to fix transposition detection
    Signed-off-by: Tom <tom@tdpain.net>

--- jaroDistance/main.py
+++ jaroDistance/main.py
@@ -1,22 +1,3 @@
-def is_match_present(source_char, search_list, current_index, dist):
-  lower_bound = current_index - dist
-  upper_bound = current_index + dist + 1  # Ensure value at current_index is included
-
-  # Prevent IndexErrors
-  if lower_bound < 0:
-    lower_bound = 0
-  if upper_bound > len(search_list):
-    upper_bound = len(search_list)
-
-  for i, c in enumerate(search_list):
-    if i < lower_bound or i >= upper_bound:
-      continue
-    elif source_char == c:
-      search_list.pop(i)
-      return True
-
-  return False
-
 def pad_arrays(l1, l2):
   l1_len = len(l1)
   l2_len = len(l2)
@@ -51,24 +32,19 @@ def jaro_similarity(s1:str, s2:str) -> float:
 
   for i in range(s1_len):
     lower_bound = max(0, i - match_distance_requirement)
-    upper_bound = min(s2_len, i + match_distance_requirement)
+    upper_bound = min(s2_len, i + match_distance_requirement + 1)
     
     for x in range(lower_bound, upper_bound):
-      print(lower_bound, "-", upper_bound, "@", x, s1[i], s2[x], s1[i] == s2[x])
+      print(lower_bound, "-", upper_bound, "@", x, f"({i})", s1[i], s2[x], s1[i] == s2[x])
       if s2_matches[x]:  # Already matched to something, so cannot use
         continue
-        print("This triggered")
       elif s1[i] == s2[x]:
         s1_matches[i] = True
         s2_matches[x] = True
-
-        print(s1_matches, s2_matches)
-
         matches += 1
-
         break  # match found, no need to keep iterating
 
-  print(s1_matches, s2_matches)
+  print(s1, s1_matches, s2, s2_matches)
 
   # Find number of transpositions
   for i in range(s1_len):
@@ -81,7 +57,19 @@ def jaro_similarity(s1:str, s2:str) -> float:
           transpositions += 1
         s2_matches[x] = False
 
+  # k = 0
+  # for i in range(s1_len):
+  #   if not s1_matches[i]:
+  #     continue
+  #   while not s2_matches[k]:
+  #     k += 1
+  #   if s1[i] != s2[k]:
+  #     transpositions += 1
+  #   k += 1
+
   transpositions /= 2
+
+  print("m", matches, "t", transpositions)
 
   # Calulate similarity
   sim = 1/3*((matches/s1_len) + (matches/s2_len) + ((matches-transpositions)/matches)) if matches != 0 else 0.0
